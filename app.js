b9f6224a941e0161a27210f923fd368ec6d69d80
Nithin Prakash <nithinpnandanam@gmail.com>
2022-10-31 00:58:13 +0530

    added dummy data to JSON and done some UI changes

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
+// Clicking add new employee button
 let add_new_btn=document.getElementById("add_new")
 add_new_btn.addEventListener("click", modal_box_add);
-
 function modal_box_add(){
     let modal_box_add=document.getElementById("modal_box_add")
     modal_box_add.style.display="block"
 }
+//closng after adding new employee
 let add_cross=document.getElementById("add_cross")
 add_cross.addEventListener("click", modal_box_close);
 function modal_box_close(){
     let modal_box_add=document.getElementById("modal_box_add")
     modal_box_add.style.display="none"
-
 }
-
-let view_icon=document.getElementById("view_icon")
-view_icon.addEventListener("click",modal_box_view)
+// Viewing details of the employee
 function modal_box_view(){
     let modal_box_view = document.getElementById("modal_box_view")
     modal_box_view.style.display="block"
-
 }
-
+// Closing after viewing details of the employee
 let view_cross=document.getElementById("view_cross")
 view_cross.addEventListener("click", modal_view_close);
 function modal_view_close(){
     let modal_box_view = document.getElementById("modal_box_view")
     modal_box_view.style.display="none"
-
 }
 
-let delete_icon = document.getElementById("delete_icon")
-delete_icon.addEventListener("click",modal_delete)
+// for showing delete confirmation box
 function modal_delete(){
     let modal_box_delete= document.getElementById("modal_box_delete")
     modal_box_delete.style.display="block"
+    // modal_box_delete.style.transition="transform 5s"
+}
+
+// for hiding delete confirmatio box 
+let delete_cross=document.getElementById("delete_cross")
+delete_cross.addEventListener("click", modal_confirmation_close);
+function modal_confirmation_close(){
+    let modal_box_delete = document.getElementById("modal_box_delete")
+    modal_box_delete.style.display="none"
 }
 
     
