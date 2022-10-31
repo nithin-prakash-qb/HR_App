58cea75f20d28e33ef1a1c6192b01bb0a3181156
Nithin Prakash <nithinpnandanam@gmail.com>
2022-11-01 00:49:32 +0530

    loaded data from JSON for initial viewing of data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const getDataLocation = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data)
+        // console.log(data)
         localStorage.setItem("empoyeeData", JSON.stringify(data));
       });
   };
@@ -66,7 +66,7 @@ main_table.setAttribute("id","main_table")
 
 let t_r_1=document.createElement("tr")
 let t_h_1=document.createElement("th")
-t_h_1.innerHTML="Employee"
+t_h_1.innerHTML="Employee ID"
 let t_h_2=document.createElement("th")
 t_h_2.innerHTML="Name"
 let t_h_3=document.createElement("th")
@@ -81,11 +81,61 @@ function addInitialData(){
     const location = JSON.parse(localStorage.getItem("empoyeeData"));
     location["details"].forEach(element => {
         let table_row=document.createElement("tr")
-        let table_data=document.createElement("td")
-        table_data.innerHTML="element[""]
+        for(let i=1;i<=5;i++){
+            let table_data=document.createElement("td")
+            if(i===1){
+                table_data.innerHTML=`${element.employee_id}`
+                table_row.appendChild(table_data)
+            } else if(i===2){
+                table_data.innerHTML=`${element.name}`
+                table_row.appendChild(table_data)
+            }else if(i===3){
+                table_data.innerHTML=`${element.email_id}`
+                table_row.appendChild(table_data)
+            }else if(i===4){
+                let table_data=document.createElement("td")
+                let count_skills=element["skills"].length
+                element["skills"].forEach((element,index) => {
+                    let table_data_new=document.createElement("p")
+                    table_data_new.setAttribute("class","inline_prop")
+                    // return(count_skills-1===index?table_data_new.innerHTML=` ${element}`:table_data_new.innerHTML=` ${element} , `)
+            
+                    if(count_skills-1===index){
+                        table_data_new.innerHTML=` ${element}`
+                    }else{
+                        table_data_new.innerHTML=` ${element} , `
+                    }
+                    table_data.appendChild(table_data_new)
+                });
+                table_row.appendChild(table_data)
 
+            }
+            else {
+                let icons=document.createElement("div")
+                icons.setAttribute("class","icons")
+                let sub_icon_1=document.createElement("i")
+                sub_icon_1.setAttribute("class","fa-solid fa-eye")
+                sub_icon_1 .setAttribute("onclick",modal_box_view);
+                sub_icon_1 .onclick= ()=> modal_box_view();
+                let sub_icon_2=document.createElement("i")
+                sub_icon_2.setAttribute("class","fa-solid fa-pen-to-square")
+                let sub_icon_3=document.createElement("i")
+                sub_icon_3.setAttribute("class","fa-solid fa-trash")
+                sub_icon_3 .setAttribute("onclick",modal_delete);
+                sub_icon_3 .onclick= ()=> modal_delete();
+                icons.appendChild(sub_icon_1)
+                icons.appendChild(sub_icon_2)
+                icons.appendChild(sub_icon_3)
+                table_data.appendChild(icons)
+                table_row.appendChild(table_data)
+            }
+        }
+        main_table.appendChild(table_row)   
     });
+     
 }
+
+
 
 
 
@@ -106,11 +156,12 @@ filter.appendChild(option_b_2)
 filter.appendChild(option_b_3)
 parent.appendChild(main_table)
 main_table.appendChild(t_r_1)
+t_r_1.appendChild(t_h_1)
 t_r_1.appendChild(t_h_2)
 t_r_1.appendChild(t_h_3)
 t_r_1.appendChild(t_h_4)
 t_r_1.appendChild(t_h_5)
-
+addInitialData()
 
 
 
